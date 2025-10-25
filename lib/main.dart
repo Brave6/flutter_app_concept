a6babdba22aa85d1e2cce1906ef5a84658f2ef9f
kurooo-yamiii <kennethcobarubias12@gmail.com>
2025-10-25 15:37:36 +0800

    Test Branch

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Test Push Tangina Ka'),
+            const Text('Main'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
